bf94a5603dd44dd796749d4c53874959db9a4482
Alpay Oğuzhan Palabıyık <alpayoguz97@gmail.com>
2022-07-01 12:27:09 +0300

    first comit

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  formElement: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.formElement = importedNode.firstElementChild as HTMLFormElement;
+    this.formElement.id = "user-input";
+    this.attach();
+  }
+  attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
+  }
+}
+
+const newIns = new ProjectInput();
