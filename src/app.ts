6718acabcb9582e8c047320bac5bef569feed426
Alpay Oğuzhan Palabıyık <alpayoguz97@gmail.com>
2022-07-01 16:27:13 +0300

    add form validation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
+
+
+//Project Input Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -22,15 +25,37 @@ class ProjectInput {
     this.descriptionInputElement = this.formElement.querySelector("#description")! as HTMLInputElement;
     this.peopleInputElement = this.formElement.querySelector("#people")! as HTMLInputElement;
     
-
     this.configure();
     this.attach();
-
-
   }
+
+  private gatherInput() : [string, string, number] | void{
+    const gatheredTitle = this.titleInputElement.value
+    const gatheredDescription = this.descriptionInputElement.value
+    const gatheredPeople = this.peopleInputElement.value;
+    if(gatheredTitle.trim() === "" || gatheredDescription.trim() === "" || gatheredPeople.trim() === ""){
+      alert("Please fill all areas")
+      return;
+    }else{
+      return [gatheredTitle, gatheredDescription, +gatheredPeople]
+    }
+  }
+
+  private clearInput(){
+    this.titleInputElement.value = ""
+    this.peopleInputElement.value = ""
+    this. descriptionInputElement.value = ""
+  }
+
   submitHandler(event: Event){
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const formInputs = this.gatherInput();
+    if(Array.isArray(formInputs)){
+      const [title, description, people] = formInputs
+      console.log(title, description, people)
+      this.clearInput()
+    }
+   
   }
   configure(){
     this.formElement.addEventListener("submit", this.submitHandler.bind(this))
