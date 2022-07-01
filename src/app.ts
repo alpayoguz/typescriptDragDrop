bfdc24136686175bc1f3f8c6fa50e85ed530ee16
Alpay Oğuzhan Palabıyık <alpayoguz97@gmail.com>
2022-07-01 14:16:45 +0300

    add input elements

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   formElement: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -13,12 +17,30 @@ class ProjectInput {
       true
     );
     this.formElement = importedNode.firstElementChild as HTMLFormElement;
-    this.formElement.id = "userg-input";    
+    this.formElement.id = "user-input";    
+    this.titleInputElement = this.formElement.querySelector("#title")! as HTMLInputElement;
+    this.descriptionInputElement = this.formElement.querySelector("#description")! as HTMLInputElement;
+    this.peopleInputElement = this.formElement.querySelector("#people")! as HTMLInputElement;
+    
+
+    this.configure();
     this.attach();
+
+
+  }
+  submitHandler(event: Event){
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+  configure(){
+    this.formElement.addEventListener("submit", this.submitHandler.bind(this))
   }
   attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
   }
+
+
+
 }
 
 const newIns = new ProjectInput();
